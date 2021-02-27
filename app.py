a76a9a6529c4a6780d6a467e3c90cd46c1710c42
Rick Goshen <rick.goshen@gmail.com>
2021-02-27 11:47:54 -0700

    added basic flask application

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask_bootstrap import Bootstrap
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///blogly"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'phillyflyerswillbechampions'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-connect_db(app)
-db.create_all()
+bootstrap = Bootstrap(app)
+toolbar = DebugToolbarExtension(app)
+
+
+# connect_db(app)
+# db.create_all()
