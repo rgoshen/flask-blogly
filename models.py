038f2dc1ab1d9cba5afabe3d2318cf0cf02a2ef8
Rick Goshen <rick.goshen@gmail.com>
2021-02-27 11:11:10 -0700

    setup User model

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.Text, nullabe=False, default=DEFAULT_IMG_URL)
