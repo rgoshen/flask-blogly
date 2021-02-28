94402dcc4df8c27806da46bf79d038ee8e204c52
Rick Goshen <rick.goshen@gmail.com>
2021-02-27 21:27:12 -0700

    added edit and delete

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ toolbar = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+
 @app.route('/')
 def root():
     """Homepage redirects to list of users."""
 
     return redirect("/users")
+
 
 @app.route('/users')
 def users_index():
@@ -31,11 +33,13 @@ def users_index():
     users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users/index.html', users=users)
 
+
 @app.route('/users/new', methods=["GET"])
 def users_new_form():
     """Show a form to create a new user"""
 
     return render_template('users/new.html')
+
 
 @app.route("/users/new", methods=["POST"])
 def users_new():
@@ -47,6 +51,48 @@ def users_new():
         image_url=request.form['image_url'] or None)
 
     db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show a page with info on a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show a form to edit an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Handle form submission for updating an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
     db.session.commit()
 
     return redirect("/users")
